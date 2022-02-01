d76e55d5fa925cc9e1d37529d5dc35d840c7e8eb
kenmiya4869 <kenmiya.works@gmail.com>
2022-02-01 15:53:14 +0900

    python sample program.

---
+++ main.py
@@ -0,0 +1,26 @@
+def main():
+  while True:
+    print("--------------------")
+    print("function calc menues")
+    print("1: add")
+    print("2: minus")
+    print("9: exit")
+    print("--------------------")
+
+    menu_key = input("Enter Input menu number >> ")
+
+    if menu_key == 1 :
+      val1 = int(input("1st Number >> "))
+      val2 = int(input("2nd Number >> "))
+      print("%d + %d = %d" % ( val1,val2,val1+val2 ))
+
+    if menu_key == 2 :
+      val1 = int(input("1st Number >> "))
+      val2 = int(input("2nd Number >> "))
+      print("%d - %d = %d" % ( val1,val2,val1-val2 ))
+
+    elif menu_key == 9 :
+      break
+
+if __name__ == "__main__":
+    main()
